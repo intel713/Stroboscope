23e92af0c1341c3f741d971a33a3afba95362e07
intel713 <126642569+intel713@users.noreply.github.com>
2023-03-01 19:33:40 +0900

    Add files via upload

---
+++ RTC_Time_setter.ino
@@ -0,0 +1,47 @@
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
+
+ThreeWire myWire(3,2,4); // IO, SCLK, CE(DAT, CLK, RST)
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+void setup() {
+  Serial.begin(57600);
+  Rtc.Begin();
+
+  pinMode(12, INPUT_PULLUP);
+  
+  Rtc.SetIsWriteProtected(false);
+  if (!Rtc.GetIsRunning())
+    {
+        Rtc.SetIsRunning(true);
+    }
+}
+
+void loop() {
+  if(digitalRead(12) == LOW) {
+    Rtc.SetDateTime(RtcDateTime("AUG/03/2022", "23:22:10"));
+    Serial.println("set");
+  }
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+  Serial.println();
+  delay(100);
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+    char datestring[20];
+
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            dt.Month(),
+            dt.Day(),
+            dt.Year(),
+            dt.Hour(),
+            dt.Minute(),
+            dt.Second() );
+    Serial.print(datestring);
+}
