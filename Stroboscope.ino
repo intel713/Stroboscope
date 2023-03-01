efdb26778c7b8a8856081f386e04db1f23dc2eb6
intel713 <126642569+intel713@users.noreply.github.com>
2023-03-01 19:22:04 +0900

    Update and rename LED_MOSFET_Strobe_PWM.ino to Stroboscope.ino

--- Stroboscope.ino
+++ Stroboscope.ino
@@ -1,15 +1,18 @@
 #include <PWM.h>
 
+//핀 설정
 int analogPin = 2;
 int ledPin = 5;
 int modePin = 2;
+
+//PWM 스위칭 주파수 설정
 int PWMFreq = 6000;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(modePin, INPUT_PULLUP);
   
-  if(digitalRead(modePin) == HIGH){
+  if(digitalRead(modePin) == HIGH){ //스트로보스코프 모드
     long onTime = 250;
     int minDelay = 1;
     int maxDelay = 100;
@@ -23,7 +26,7 @@ void setup() {
     }
   }
   
-  else{
+  else{ //LED 밝기 제어 모드
     InitTimers();
     bool b = SetPinFrequency(ledPin, PWMFreq);
     while(0 == 0){
